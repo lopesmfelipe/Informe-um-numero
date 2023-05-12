a5089fb8f501e1f0b867e82f92beb1d90cf5c942
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-12 03:43:42 -0400

    Criado o scanner para ler os dados e os comandos de impressão

--- Informe um Numero/src/com/personal/classes/InformeUmNumero.java
+++ Informe um Numero/src/com/personal/classes/InformeUmNumero.java
@@ -6,6 +6,12 @@ public class InformeUmNumero {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe um número: ");
+		
+		
+		int number = scan.nextInt();
+		System.out.println("O número informado foi: " + number);
 		
 		
 	}
